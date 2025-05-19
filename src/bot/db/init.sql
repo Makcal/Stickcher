c0372bac8c528f7b3b854f3b5874315a5b5efb5f
Maxim Fomin <m.fomin@innopolis.university>
2025-05-19 12:59:13 +0300

    chore: add names to indexes

--- src/bot/db/init.sql
+++ src/bot/db/init.sql
@@ -2,14 +2,16 @@ CREATE TABLE IF NOT EXISTS sticker (
     file_unique_id TEXT PRIMARY KEY,
     file_id TEXT NOT NULL
 );
-CREATE INDEX ON sticker USING hash (file_id);
+CREATE INDEX IF NOT EXISTS sticker_file_id_idx ON sticker
+    USING hash (file_id);
 
 CREATE TABLE IF NOT EXISTS sticker_pack (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     owner_id BIGINT NOT NULL
 );
-CREATE INDEX ON sticker_pack USING btree (owner_id);
+CREATE INDEX IF NOT EXISTS sticker_pack_owner_id_idx ON sticker_pack
+    USING btree (owner_id);
 
 CREATE TABLE IF NOT EXISTS tag (
     text TEXT NOT NULL,
@@ -19,4 +21,5 @@ CREATE TABLE IF NOT EXISTS tag (
         ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (pack_id) REFERENCES sticker_pack(id)
 );
-CREATE INDEX ON tag USING btree (pack_id) INCLUDE (text, sticker_id);
+CREATE INDEX IF NOT EXISTS tag_pack_id_text_sticker_id_idx ON tag
+    USING btree (pack_id) INCLUDE (text, sticker_id);
