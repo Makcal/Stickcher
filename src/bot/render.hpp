8532e67862b3447c10753a1dd387cad11a80bbae
Maxim Fomin <m.fomin@innopolis.university>
2025-05-20 12:44:05 +0300

    debug: editor rights

--- src/bot/render.hpp
+++ src/bot/render.hpp
@@ -79,7 +79,7 @@ inline void renderPackView(StickerPackId packId, UserId userId, ChatId chatId, B
         isEditor = PackSharingRepository::checkEditorRights(packId, userId);
     if (isEditor)
         buttonRows = 2;
-    std::cout << isEditor << '\n';
+    std::cout << isEditor << std::endl;
 
     InlineKeyboard keyboard(buttonRows);
     keyboard[0].reserve(2);
